51adf0ef1f97231f890f9853d83ad6075688ba7d
RaissaBorgess <raissasantos123.rs33@gmail.com>
2024-03-27 14:47:46 -0300

    inicio do projetio

---
+++ CineSpotWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CineSpotWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
